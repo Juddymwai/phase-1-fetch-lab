5fcf1b0b38d4ad9a715ea98588bdf128ab009a68
Judy Mwai <juddiemukami@gmail.com>
2022-06-15 23:13:45 +0300

    commit tree ba7392a907f9885eddd58144f09164067d72b7dd

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+// const { default: fetch} = require("node-fetch")
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(json) {
+    return renderBooks(json)
+  })
 }
 
+fetchBooks()
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
